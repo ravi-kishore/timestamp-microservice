99874e4b926ab2ca334dfbab11aab95bbf2d7079
Ravi <revansh307@gmail.com>
2016-10-28 17:08:10 +0530

    Final version

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 var express = require("express");
 var path = require("path");
+var moment = require("moment");
 
 var app = express()
 app.get('/', function(req, res){
-	req.
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+var currentLocaleData = moment.localeData();
+app.get('/:date', function(req, res){
+	var date = req.params.date;
+	var momentObj;
+	if(!isNaN(date))
+		momentObj = moment.unix(date);
+	else
+		momentObj = moment(date);
+
+	var retval = {};
+	if(momentObj.isValid())
+	{
+		retval.unix = momentObj.format("X");
+		retval.natural = momentObj.format("MMMM DD, YYYY");
+	}
+	else
+	{
+		retval.unix = null;
+		retval.natural = null;
+	}
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(retval));
+});
+
 app.listen(process.env.PORT || 4000);
 
